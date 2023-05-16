ec1b13705418eeac7693d035d149727af8c4fe66
eunhwa0308 <eunhwa0308@naver.com>
2023-05-16 14:21:17 +0900

    feat: project build

---
+++ src/test/java/com/example/springbootdockerexercise2/SpringbootDockerExercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerexercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerExercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
